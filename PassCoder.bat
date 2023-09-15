e16d05bbc000dc2a3f279a9676950a4d55bf0fe8
CtrlAltWiz <46701041+CtrlAltWiz@users.noreply.github.com>
2023-09-15 10:30:45 -0700

    Create PassCoder.bat

---
+++ PassCoder.bat
@@ -0,0 +1,53 @@
+@echo off
+:Start2
+cls
+goto Start
+:Start
+title Passcoder
+echo ----------------------------------------­-----------------------
+echo You can create 1, 5, or 10 randomized number-only passcodes.
+echo Please write the new passcode(s) down in case you forget it.
+echo ----------------------------------------­-----------------------
+echo 1) 1 Random Passcode
+echo 2) 5 Random Passcode
+echo 3) 10 Random Passcode
+echo 4) Exit
+echo Input your choice
+set input=
+set /p input= Choice:
+if %input%==1 goto A if NOT goto Start2
+if %input%==2 goto B if NOT goto Start2
+if %input%==3 goto C if NOT goto Start2
+if %input%==4 goto Exit if NOT goto Start 2
+:A
+cls
+echo Your passcode is %random%
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto Start2 if NOT goto Start 2
+if %input%==2 goto Exit if NOT goto Start 2
+:Exit
+exit
+:B
+cls
+echo Your 5 passcodes are %random%, %random%, %random%, %random%, %random%.
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto Start2 if NOT goto Start 2
+if %input%==2 goto Exit if NOT goto Start 2
+:C
+cls
+echo Your 10 passcodes are %random%, %random%, %random%, %random%, %random%, %random%, %random%, %random%, %random%, %random%
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
+set input=
+set /p input= Choice:
+if %input%==1 goto Start2 if NOT goto Start 2
+if %input%==2 goto Exit if NOT goto Start 2
